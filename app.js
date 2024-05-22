571172ee0ada871b391c21c9b87cda2a08ce84ce
pasquy <pasquale.vitale@gmail.com>
2024-05-22 09:34:45 +0200

    repeat docker push

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var http = require('http');
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
-}).listen(8080);
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    const msg = 'Hello Node!\n'
+    res.end(msg);
+});
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
